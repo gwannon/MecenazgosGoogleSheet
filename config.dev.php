20809a13a85263d958bd851cade640f7294c9104
gwannon <monclus.jorge@gmail.com>
2026-04-10 12:06:06 +0200

    TimeZone para los logs

--- config.dev.php
+++ config.dev.php
@@ -4,3 +4,5 @@ define('SPREADSHEET_ID', 'XXXXXXXXXXXXXXXXXX'); //Se saca de la URL del Google S
 define('SPREADSHEET_SHEET_NAME', 'XXXXXXXXXXXX'); //Nmbre de la hoja de Google Sheet donde están los datos
 define('UPDATE_DATE', '01/01/1970'); //Fecha de última actualziación de los datos
 define('EXPIRE_CACHE', 5*60); // 5 Minutos
+
+date_default_timezone_set("Europe/Madrid");
